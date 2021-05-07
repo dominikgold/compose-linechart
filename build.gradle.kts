dc122da7656672588c70044405431f76f6a1fdcb
dominikgold <dominik.goldberg@kitchenstories.de>
2021-05-07 14:17:15 +0200

    add maven plugin to publish with jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-alpha15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath(Dependencies.kotlinPlugin)
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
 }
 
